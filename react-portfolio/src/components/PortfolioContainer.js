bb7733140d798d231765cd4b3fa28e2acd01f737
Ben Camarillo <jtby97@gmail.com>
2021-11-22 21:01:02 -0600

    created new react project and adjusted src folder

---
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import Aboutme from './pages/Aboutme';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'AboutMe') {
+      return <Aboutme />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    // return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <div>header</div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <div>footer
+      </div>
+    </div>
+  );
+}
