303c92a811dd2933402e6e769f05bf5714b6d740
Ben Camarillo <jtby97@gmail.com>
2021-12-08 16:22:13 -0600

    work on About me content in Aboutme.js

--- react-portfolio/src/components/pages/Aboutme.js
+++ react-portfolio/src/components/pages/Aboutme.js
@@ -5,12 +5,10 @@ export default function AboutMe() {
     <div>
       <h1>About Page</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Benjamin Camarillo and I have worked in customer service and admin work for 8 years. I have 6+ years of leadership and training expierence in past roles such as Shift Lead and Percussion director. In both roles I was responsable for training and comprehension of new hires. As a percussion director I was responsable for learning music programs and content creation of music material for students. 
+      </p>
+      <p>
+        I have been interested in web development since my introduction to basic game coding in high school. My expierence and interest continued into undergrad when I took a Videogame Design course. That continued fasination with problem solving and design has carried me to where I am now at the completeion of a trilogy full stack developer program.
       </p>
     </div>
   );
