d82effa532727102cb4ac7fb5eafeb6aa0ba2563
Ben Camarillo <jtby97@gmail.com>
2021-12-08 16:51:58 -0600

    changed link in Footer.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,33 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import AboutMe from "./components/pages/Aboutme";
+import Contact from "./components/pages/Contact";
 
-const App = () => <PortfolioContainer />;
+
+function App() {
+  return (
+    <>
+    
+    </>
+  );
+}
+
+// = () => <PortfolioContainer />;
 
 export default App;
+
+{/* <Router>
+  <Header>
+    <div className="content">
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/Header" element={<Header />} />
+        <Route exact path="/Footer" element={<Footer />} />
+        <Route exact path="/Aboutme" element={<AboutMe />} />
+        <Route exact path="/Contact" element={<Contact />} />
+      </Routes>
+    </div>
+  </Header>
+</Router>; */}
--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -3,9 +3,8 @@ import  "../css/footer.css";
 function Footer(){
     return (
       <div className="" style={styles.footer}>
-      <Button style={styles.link} target="_blank" href='https://github.com/Cygnet717' variant="outline-dark">Git Hub</Button>
-      <Button style={styles.link} target="_blank" href='https://www.linkedin.com/in/kathy-bradt-1796a9191/' variant="outline-dark">Linked In</Button>
-      <Button style={styles.link} target="_blank" href='https://stackoverflow.com/users/13035422/kathyb' variant="outline-dark">Stack Overflow</Button>
+      <Button style={styles.link} target="_blank" href='https://github.com/Malletmania' variant="outline-dark">Git Hub</Button>
+      <Button style={styles.link} target="_blank" href='https://www.linkedin.com/in/benny-camarillo-024a1118a/' variant="outline-dark">Linked In</Button>
       <Button style={styles.link} target="_blank" href={resume} variant="outline-dark">Resume</Button>
     </div>
     );
