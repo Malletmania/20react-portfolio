7ffe26a7c618ad3440a458c24b4b3b90428a671d
Ben Camarillo <jtby97@gmail.com>
2021-12-11 16:04:44 -0600

    Work on portfolio page including projects i've done previously

--- react-portfolio/src/pages/Portfolio.js
+++ react-portfolio/src/pages/Portfolio.js
@@ -1,24 +1,110 @@
 import React from 'react';
 
 export default function Portfolio() {
+
+  const styles={
+    img: {
+      width: '100%',
+      height: '50%',
+    },
+    card: {
+      width: '25rem',
+      boxShadow: '10px 10px 5px grey',
+      margin: '2rem'
+    },
+    header: {
+      margin: 'auto',
+      color: '#3f87a6',
+    }
+  }
+
   return (
-    <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
-    </div>
+    <>
+      <div className="d-flex">
+        <h2 style={styles.header} >Examples of My Work</h2>
+      </div> 
+      <div className="d-flex flex-wrap justify-content-center justify-space-between p-2 m-2">
+        <div className="card" style={styles.card}>
+          <img src={Bookstore} className="card-img-top" alt="screenshot of deployed code"/>
+          <div className="card-body">
+            <h5 className="card-title">Bookstore App Group Project</h5>
+            <p className="card-text"> Tech Used: <br/>
+            Node<br/>
+             Express <br/>
+             Express-Handlebars <br/>
+             Sequelize <br/>
+             MySQl2</p>
+          </div>
+          <div className="card-body">
+            <a href="https://github.com/Malletmania/book-store" target="_blank" rel="noreferrer noopener" className="card-link">Github Repo</a>
+            <a href="https://kesjoberg-bookstore.herokuapp.com/" target="_blank" rel="noreferrer noopener" className="card-link">Deployed Code</a>
+          </div>
+        </div>
+        <div className="card" style={styles.card}>
+          <img src={Bookstore} className="card-img-top" alt="screenshot of deployed code"/>
+          <div className="card-body">
+            <h5 className="card-title">Northern Cocktail Viewer App</h5>
+            <p className="card-text"> Tech Used: <br/>
+            Node<br/>
+             HTML <br/>
+             CSS <br/>
+             JavaScript <br/>
+             </p>
+          </div>
+          <div className="card-body">
+            <a href="https://github.com/Malletmania/northern_cocktail_viewer" target="_blank" rel="noreferrer noopener" className="card-link">Github Repo</a>
+          </div>
+        </div>
+        <div className="card" style={styles.card}>
+          <img src={Employee} className="card-img-top" alt="screenshot of deployed code"/>
+          <div className="card-body">
+            <h5 className="card-title">Employee Management System</h5>
+            <p className="card-text"> Tech Used:<br/>
+             Node<br/>
+             Express <br/>
+             Inquirer <br/>
+             Console.Table <br/>
+             MySQl2</p>
+          </div>
+          <div className="card-body">
+            <a href="https://github.com/Malletmania/12-Employee_tracker" target="_blank" rel="noreferrer noopener" className="card-link">Github Repo</a>
+            <a href="https://drive.google.com/file/d/1tyEx0A_OlzfFYYchogMCA4c3ZMhBxhew/view" target="_blank" rel="noreferrer noopener" className="card-link">Video Demo</a>
+          </div>
+        </div>
+        <div className="card" style={styles.card}>
+          <img src={Team} className="card-img-top" alt="screenshot of deployed code"/>
+          <div className="card-body">
+            <h5 className="card-title">Team Builder App</h5>
+            <p className="card-text"> Tech Used: <br/>
+            Node<br/>
+            Inquirer <br/>
+             Jest <br/>
+             Bootstrap</p>
+          </div>
+          <div className="card-body">
+            <a href="https://github.com/Malletmania/10-Profile-generator" target="_blank" rel="noreferrer noopener" className="card-link">Github Repo</a>
+          </div>
+        </div>
+        <div className="card" style={styles.card}>
+          <img src={Bookstore} className="card-img-top" alt="screenshot of deployed code"/>
+          <div className="card-body">
+            <h5 className="card-title">Video Game Storage Shelf App Group Project</h5>
+            <p className="card-text"> Tech Used: <br/>
+            apollo-server-express<br/>
+             Express <br/>
+             Bootstrap <br/>
+             cors <br/>
+             react-bootstrap <br/>
+             react-router-dom <br/>
+             Mongoose <br/>
+             Bcrypt</p>
+          </div>
+          <div className="card-body">
+            <a href="https://github.com/Malletmania/group-project-3" target="_blank" rel="noreferrer noopener" className="card-link">Github Repo</a>
+          </div>
+        </div>
+      </div>
+    </>
+
   );
 }
